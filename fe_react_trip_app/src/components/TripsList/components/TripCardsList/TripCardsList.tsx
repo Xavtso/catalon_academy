bf9d164a65ff87e863597583898886a0375ac266
Vitaliy Havrona <vitaliyhavrona@gmail.com>
2024-02-20 14:12:42 +0200

    trips media added

--- fe_react_trip_app/src/components/TripsList/components/TripCardsList/TripCardsList.tsx
+++ fe_react_trip_app/src/components/TripsList/components/TripCardsList/TripCardsList.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllTrips } from "../../../../actions/tripsAction";
 import { AppDispatch, PlaceType, RootState } from "../../../../types";
+import { url } from "inspector";
 
 export default function TripCardsList() {
   const dispatch = useDispatch<AppDispatch>();
@@ -9,18 +10,30 @@ export default function TripCardsList() {
     (state: RootState) => state.trips.continents,
   );
 
+  
+
   useEffect(() => {
     dispatch(getAllTrips());
   }, [dispatch]);
 
   return (
     <div>
-      {Africa.map((place: PlaceType) => (
-        <div key={place.id}>
+      {Europe.map((place: PlaceType, index) => (
+        <div key={index}>
           <h1>{place.city}</h1>
-          <h2>{place.Dates}</h2>
-          <p>{place.TotalPrice}</p>
-          <p>{place.Detailed}</p>
+          <h2>{place.dates}</h2>
+          
+          <div
+            style={{
+              backgroundImage: `url(${place.imageSrc})`,
+              width: "360px",
+              height: "180px",
+              backgroundSize: 'cover',
+              backgroundPosition:'center'
+            }}
+          ></div>
+          <p>{place.totalPrice}</p>
+          <p>{place.detailed}</p>
         </div>
       ))}
     </div>
--- fe_react_trip_app/src/types/index.ts
+++ fe_react_trip_app/src/types/index.ts
@@ -4,9 +4,9 @@ export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
 export type PlaceType = {
-  id: number;
   city: string;
-  Detailed: string;
-  Dates: string;
-  TotalPrice: number;
+  detailed: string;
+  dates: string;
+  totalPrice: number;
+  imageSrc: string;
 };
